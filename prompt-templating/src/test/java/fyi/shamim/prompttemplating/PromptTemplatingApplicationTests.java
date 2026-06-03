bcbd3e1ad43cf283a49a041e48cd8f3563265471
Md <mdshamim723@gmail.com>
2026-06-03 22:45:54 +0600

    prompt templating demo added

---
+++ prompt-templating/src/test/java/fyi/shamim/prompttemplating/PromptTemplatingApplicationTests.java
@@ -0,0 +1,13 @@
+package fyi.shamim.prompttemplating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptTemplatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
