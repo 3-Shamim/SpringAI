153977f2edb07ea7290d329f9eac7dfbffc02f10
Md <mdshamim723@gmail.com>
2026-06-08 07:33:20 +0600

    complete with SringAI v1

---
+++ chat-memory-with-springai-v1/src/main/java/fyi/shamim/chatmemorywithspringaiv1/ChatMemoryWithSpringaiV1Application.java
@@ -0,0 +1,13 @@
+package fyi.shamim.chatmemorywithspringaiv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMemoryWithSpringaiV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatMemoryWithSpringaiV1Application.class, args);
+    }
+
+}
---
+++ chat-memory-with-springai-v1/src/test/java/fyi/shamim/chatmemorywithspringaiv1/ChatMemoryWithSpringaiV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fyi.shamim.chatmemorywithspringaiv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMemoryWithSpringaiV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
